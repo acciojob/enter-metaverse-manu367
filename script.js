1d8272d2b9f1fe2b147510e1ee18166b8d135f32
Manu Pathak <83893814+manu367@users.noreply.github.com>
2025-07-02 22:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const button=document.getElementById("button");
+const heading=document.getElementById("heading");
+button.addEventListener("click",()=>{
+	heading.innerHTML="Entered Metaverse";
+});
